59978128d441ad55a94f8b29c319cb60f890cbbf
khiladi1314 <152869573+khiladi1314@users.noreply.github.com>
2024-03-04 12:23:04 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  } 
+  
+  backend "s3" {
+    bucket         	   = "terraform-tfstat-bucket2439"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "ap-southeast-1"
+    dynamodb_table = "terraform_lock"
+  }
+}
