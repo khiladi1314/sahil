8da6c7c320fa81f0f5271d4a55223f5e9ad39685
khiladi1314 <152869573+khiladi1314@users.noreply.github.com>
2024-03-04 12:06:52 +0530

    Update output.tf

--- security_group/output.tf
+++ security_group/output.tf
@@ -1,4 +1,4 @@
 output "security_group_output" {
-  value = aws_security_group.ES_security_group.id
+  value = aws_security_group.kafka_security_group.id
   description = "this is the output for security group"
 }
