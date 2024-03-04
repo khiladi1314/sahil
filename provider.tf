e8a29895387fd701c1b72a06d49b8d71f3f69210
khiladi1314 <152869573+khiladi1314@users.noreply.github.com>
2024-03-04 12:43:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = var.root_region
 }
+
+provider "aws" {
+  alias  = "jenkins"
+  region = "ap-south-1"
+}
+
+provider "aws" {
+  alias  = "peer"
+  region = "ap-southeast-1"
+}
